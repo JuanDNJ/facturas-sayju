54bac65814b069fc6775fa6fd30a7cb2e41c79ff
juandevnjv <juandevnjv@gmail.com>
2025-10-25 18:24:07 +0000

    fix: add svg under-construction

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -69,11 +69,60 @@ export default function Dashboard() {
   }, [user])
   return (
     <section>
-      <div className="panel mb-4 flex items-center gap-2 rounded p-3">
-        <span aria-hidden>🚧</span>
-        <span className="muted text-sm">
-          Esta página está en construcción. Las métricas son ficticias y están sujetas a cambios.
-        </span>
+      <div className="text-under-construction mb-4 gap-2 rounded p-3">
+        <section className="flex flex-row">
+          <article className='flex justify-center items-center gap-2 p-3'>
+            <span aria-hidden className='max-w-14 md:max-w-32'>
+              <svg
+                fill="currentColor"
+                height="100%"
+                width="100%"
+                version="1.1"
+                id="Capa_1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="-4.92 -4.92 42.65 42.65"
+                stroke="#F3EDD7"
+                strokeWidth="0.00032811000000000006"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0" />
+
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  stroke="#050505"
+                  strokeWidth="5.381003999999999"
+                >
+                  {' '}
+                  <g>
+                    {' '}
+                    <path d="M0,1.356v16.743h5.249v7.154v3.617v2.586h3.61v-2.586h15.423v2.586h3.606V18.098h4.923V1.356H0z M31.136,2.281 l-7.764,14.658h-4.783l7.762-14.658C26.351,2.281,31.136,2.281,31.136,2.281z M23.016,2.245l-7.766,14.66h-4.785l7.767-14.66 C18.232,2.245,23.016,2.245,23.016,2.245z M1.31,2.429l5.617-0.017L1.281,12.99L1.31,2.429z M7.468,17.06H2.684l7.763-14.658h4.785 L7.468,17.06z M24.282,25.252H8.859v-7.154h15.423C24.282,18.098,24.282,25.252,24.282,25.252z M32.149,16.811l-5.616-0.006 l5.646-10.576L32.149,16.811z" />{' '}
+                    <path d="M0,1.356v16.743h5.249v7.154v3.617v2.586h3.61v-2.586h15.423v2.586h3.606V18.098h4.923V1.356H0z M31.136,2.281 l-7.764,14.658h-4.783l7.762-14.658C26.351,2.281,31.136,2.281,31.136,2.281z M23.016,2.245l-7.766,14.66h-4.785l7.767-14.66 C18.232,2.245,23.016,2.245,23.016,2.245z M1.31,2.429l5.617-0.017L1.281,12.99L1.31,2.429z M7.468,17.06H2.684l7.763-14.658h4.785 L7.468,17.06z M24.282,25.252H8.859v-7.154h15.423C24.282,18.098,24.282,25.252,24.282,25.252z M32.149,16.811l-5.616-0.006 l5.646-10.576L32.149,16.811z" />{' '}
+                    <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g>{' '}
+                    <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g>{' '}
+                  </g>{' '}
+                </g>
+
+                <g id="SVGRepo_iconCarrier">
+                  {' '}
+                  <g>
+                    {' '}
+                    <path d="M0,1.356v16.743h5.249v7.154v3.617v2.586h3.61v-2.586h15.423v2.586h3.606V18.098h4.923V1.356H0z M31.136,2.281 l-7.764,14.658h-4.783l7.762-14.658C26.351,2.281,31.136,2.281,31.136,2.281z M23.016,2.245l-7.766,14.66h-4.785l7.767-14.66 C18.232,2.245,23.016,2.245,23.016,2.245z M1.31,2.429l5.617-0.017L1.281,12.99L1.31,2.429z M7.468,17.06H2.684l7.763-14.658h4.785 L7.468,17.06z M24.282,25.252H8.859v-7.154h15.423C24.282,18.098,24.282,25.252,24.282,25.252z M32.149,16.811l-5.616-0.006 l5.646-10.576L32.149,16.811z" />{' '}
+                    <path d="M0,1.356v16.743h5.249v7.154v3.617v2.586h3.61v-2.586h15.423v2.586h3.606V18.098h4.923V1.356H0z M31.136,2.281 l-7.764,14.658h-4.783l7.762-14.658C26.351,2.281,31.136,2.281,31.136,2.281z M23.016,2.245l-7.766,14.66h-4.785l7.767-14.66 C18.232,2.245,23.016,2.245,23.016,2.245z M1.31,2.429l5.617-0.017L1.281,12.99L1.31,2.429z M7.468,17.06H2.684l7.763-14.658h4.785 L7.468,17.06z M24.282,25.252H8.859v-7.154h15.423C24.282,18.098,24.282,25.252,24.282,25.252z M32.149,16.811l-5.616-0.006 l5.646-10.576L32.149,16.811z" />{' '}
+                    <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g>{' '}
+                    <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g>{' '}
+                  </g>{' '}
+                </g>
+              </svg>
+            </span>
+          </article>
+          <article className='flex-1 flex items-center justify-center gap-2 p-3'>
+            <span className="text-xs md:text-4xl text-primary font-weight-black">
+              Esta página está en construcción. Las métricas son ficticias y están sujetas a
+              cambios.
+            </span>
+          </article>
+        </section>
       </div>
       <div className="panel mb-4 flex items-center justify-between gap-3 rounded p-3">
         <div className="flex items-center gap-3">
