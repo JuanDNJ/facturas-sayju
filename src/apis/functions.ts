32c30afb2504fccbbd50f3134ea36fc4d87ef473
SEJAps <matchdaystatsmds@gmail.com>
2025-10-26 08:42:16 +0100

    refactor: eliminar punto y coma al final de las declaraciones en functions.ts

--- src/apis/functions.ts
+++
@@ -1,7 +0,0 @@
-import { getFunctions, httpsCallable } from 'firebase/functions';
-import { appFirebase } from './firebase';
-
-const functions = getFunctions(appFirebase);
-
-export const helloWorld = httpsCallable(functions, 'helloWorld');
-export const getServerTimestamp = httpsCallable(functions, 'getServerTimestamp');
